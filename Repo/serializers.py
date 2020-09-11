ccc970f4cc553374a2da9d1b277be4d4807da389
RinkeshSante <52735633+rinkeshsante@users.noreply.github.com>
2020-09-11 21:01:49 +0530

    added rest api for computer,soft,epq,perchase,

--- Repo/serializers.py
+++ Repo/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from .models import Department, Computer, Equipment, Lab, Software
+from .models import Department, Computer, Equipment, Lab, Software, Perchase
+
+# this page will be used to decide the fields in api json
 
 
 class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,4 +14,28 @@ class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
 class LabSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lab
-        fields = ('id', 'name', 'department')
+        fields = ('id', 'name', 'code', 'lab_number')
+
+
+class ComputerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Computer
+        fields = ('id', 'name', 'Computer_no', 'ram', 'storage')
+
+
+class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Equipment
+        fields = ('id', 'name', 'equipment_no', 'code')
+
+
+class SoftwareSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Software
+        fields = ('id', 'name', 'code', 'software_no')
+
+
+class PerchaseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Perchase
+        fields = ('id', 'bill_no', 'supplier', 'rate')
--- Repo/views.py
+++ Repo/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 
-from .serializers import DepartmentSerializer, LabSerializer
-from .models import Department, Lab
+from .serializers import DepartmentSerializer, LabSerializer, ComputerSerializer, SoftwareSerializer, PerchaseSerializer, EquipmentSerializer
+from .models import Department, Lab, Computer, Software, Perchase, Equipment
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
@@ -15,3 +15,23 @@ class DepartmentViewSet(viewsets.ModelViewSet):
 class labViewSet(viewsets.ModelViewSet):
     queryset = Lab.objects.all().order_by('id')
     serializer_class = LabSerializer
+
+
+class ComputerViewSet(viewsets.ModelViewSet):
+    queryset = Computer.objects.all().order_by('id')
+    serializer_class = ComputerSerializer
+
+
+class SoftwareViewSet(viewsets.ModelViewSet):
+    queryset = Software.objects.all().order_by('id')
+    serializer_class = SoftwareSerializer
+
+
+class PerchaseViewSet(viewsets.ModelViewSet):
+    queryset = Perchase.objects.all().order_by('id')
+    serializer_class = PerchaseSerializer
+
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    queryset = Equipment.objects.all().order_by('id')
+    serializer_class = EquipmentSerializer
